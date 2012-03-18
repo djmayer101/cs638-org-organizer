dc24783f85a986eafa119cc1838bd8c23af893e2
Jim <jim@ubuntu.(none)>
2012-03-18 15:29:14 -0500

    updated events attributes

--- db/migrate/20120316205700_create_events.rb
+++ db/migrate/20120316205700_create_events.rb
@@ -3,7 +3,11 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :title
       t.text :description
-
+      t.datetime :start_date
+      t.datetime :end_date
+      t.string  :formality_lvl
+      t.integer :owner #User_id
+      t.string  :location
       t.timestamps
     end
   end

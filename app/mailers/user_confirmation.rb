519ca5c833ff8bb8b64c7fbed5fb75e79b89e337
Jim <jim@ubuntu.(none)>
2012-03-18 22:32:03 -0500

    Added the logic to generate random password. Also finished email confirmation.

--- app/mailers/user_confirmation.rb
+++ app/mailers/user_confirmation.rb
@@ -1,3 +1,9 @@
 class UserConfirmation < ActionMailer::Base
-  default from: "from@example.com"
+  default :from => "notifications@example.com"
+ 
+  def registration_email(user)
+    @user = user
+    @url  = "http://example.com/login"
+    mail(:to => user.email, :subject => "Invited to join our OrgOrganizer")
+  end
 end

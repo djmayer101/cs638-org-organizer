59328d47c550d64e8c651dd21dda5b2fb3ba44b7
Drew Smith <dcsmith35@drew-smiths-macbook-2.local>
2012-03-21 16:30:33 -0500

    Fixing migration problems.

--- db/migrate/20120316205618_create_duties.rb
+++ db/migrate/20120316205618_create_duties.rb
@@ -3,11 +3,6 @@ class CreateDuties < ActiveRecord::Migration
     create_table :duties do |t|
       t.string  :title
       t.text    :description
-      t.date    :deadline
-      t.integer :assignee #use the user's id
-      t.integer :assigner
-      t.string  :penalty
-      t.boolean :confirmation
       t.timestamps
     end
   end

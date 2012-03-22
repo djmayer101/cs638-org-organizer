c1616d66d82328d69febb7714364d526e327e763
David Mayer <djmayer101@gmail.com>
2012-03-21 22:54:53 -0500

    modified testing files

--- test/unit/spec_helper.rb
+++ test/unit/spec_helper.rb
@@ -1,6 +1,6 @@
-require_relative '../app/models/user.rb'
-require_relative '../app/models/postion.rb'
-require_relative '../app/models/duty.rb'
-require_relative '../app/models/event.rb'
+require_relative '../../app/controllers/users_controller.rb'
+#require_relative '../app/models/postion.rb'
+#require_relative '../app/models/duty.rb'
+#require_relative '../app/models/event.rb'
 
 require 'yaml'
--- test/unit/user1_test.rb
+++ test/unit/user1_test.rb
@@ -1,17 +1,18 @@
-require 'spec_helper.rb'
+require './spec_helper.rb'
 
 describe User do
 
 #Before each test runs, run this:
-  before :each do
-    @user = User.new "John", "Doe" , "BigDoe@gmail.com", "hunter2" 
-  end
+#  before :each do
+#    @user = User.new "John", "Doe" , "BigDoe@gmail.com", "hunter2" 
+#  end
 
 
   #t’s a convention in Ruby to talk refer to instance methods 
   #like this: ClassName#methodName 
-  describe "#new" do
+  describe  do
     it "Takes 4 params and returns a User object" do
+      @user = User.new "John", "Doe" , "BigDoe@gmail.com", "hunter2", false 
       @user.should be_an_instance_of User
     end
   end

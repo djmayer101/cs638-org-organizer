d80d89b784827ee7f3848a0de2eb9661c252a57b
David Mayer <djmayer101@gmail.com>
2012-03-21 21:11:06 -0500

    added scaffolding so that users can be made into admins

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :admin
   has_many  :positions, :through => :position_assignments
   has_many  :duties,    :through => :duty_assignments
   has_many  :events

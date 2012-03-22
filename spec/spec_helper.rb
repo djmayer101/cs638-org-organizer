2810d9cf2fd5e7751e9f119f09bb82bb426533f4
David Mayer <djmayer101@gmail.com>
2012-03-22 16:37:20 -0500

    added rspec files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require_relative '../app/models/user.rb'
+require_relative '../app/models/postion.rb'
+require_relative '../app/models/duty.rb'
+require_relative '../app/models/event.rb'
+
+require 'yaml'

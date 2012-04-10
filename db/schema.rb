10766deab41437869aea9227dfb2a1b5060e5b1a
Brennan Payne <brennan@brennan-Studio-1555.(none)>
2012-04-10 18:53:05 -0500

    Added an assignment page for positions.  Does not funtion yet.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120408062038) do
+ActiveRecord::Schema.define(:version => 20120410230133) do
 
   create_table "duties", :force => true do |t|
     t.string   "title"
@@ -53,6 +53,11 @@ ActiveRecord::Schema.define(:version => 20120408062038) do
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "positions_users", :id => false, :force => true do |t|
+    t.integer "positions_id"
+    t.integer "users_id"
   end
 
   create_table "users", :force => true do |t|

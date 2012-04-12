52a4e050a5d52cca08d21f260e985afccd5785ce
David Mayer <djmayer101@gmail.com>
2012-04-11 20:45:10 -0500

    fixed broken update event condition

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event <ActiveRecord::Base
   belongs_to  :user
-  has_and_belongs_to_many :users
+ # has_and_belongs_to_many :users
   validates :title, :description, :location, :owner, :presence=> true
   validates :formality_lvl, :start_date, :end_date, :presence=> true
   validates :owner, :numericality => true;
@@ -8,7 +8,7 @@ class Event <ActiveRecord::Base
   validate :after_start_date
   
   def after_start_date
-        errors.add :base, ("End Date must be after the Start Date") unless end_date.to_f > start_date.to_f
+    errors.add :base, ("End Date must be after the Start Date") unless end_date.to_f > start_date.to_f
   end
 
 end

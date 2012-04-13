e23d51ba36d9fe4a5eb8110010faa1ae8b6ac697
David Mayer <djmayer101@gmail.com>
2012-04-12 20:46:05 -0500

    removed unneeded factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,12 +7,6 @@ FactoryGirl.define do
    admin    true
   end
 
-  factory :user_with_duty, :parent => :user do
-    after_create do |u|
-	FactoryGirl.create(:duty, :user => u)
-    end
-  end
-
   factory :duty, class: Duty do 
    title	"my Duty"
    description	"clean"

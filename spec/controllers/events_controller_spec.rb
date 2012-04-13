293368426fa2b4be0bf3b9183363cd2be8c97a11
David Mayer <djmayer101@gmail.com>
2012-04-12 21:35:42 -0500

    removed unneeded valid_session from events tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -17,7 +17,7 @@ describe EventsController do
   describe "GET index" do
     it "assigns all events as @events" do
       event = Event.create! valid_attributes
-      get :index, {}, valid_session
+      get :index, {}
       assigns(:events).should eq([event])
     end
   end
@@ -25,14 +25,14 @@ describe EventsController do
   describe "GET show" do
     it "assigns the requested event as @event" do
       event = Event.create! valid_attributes
-      get :show, {:id => event.to_param}, valid_session
+      get :show, {:id => event.to_param}
       assigns(:event).should eq(event)
     end
   end
 
   describe "GET new" do
     it "assigns a new event as @event" do
-      get :new, {}, valid_session
+      get :new, {}
       assigns(:event).should be_a_new(Event)
     end
   end
@@ -40,7 +40,7 @@ describe EventsController do
   describe "GET edit" do
     it "assigns the requested event as @event" do
       event = Event.create! valid_attributes
-      get :edit, {:id => event.to_param}, valid_session
+      get :edit, {:id => event.to_param}
       assigns(:event).should eq(event)
     end
   end
@@ -49,25 +49,25 @@ describe EventsController do
     describe "with valid params" do
       it "creates a new Event" do
         expect {
-          post :create, {:event => valid_attributes}, valid_session
+          post :create, {:event => valid_attributes}
         }.to change(Event, :count).by(1)
 	#cleanup google calendar
-	delete :destroy, {:id => Event.last.to_param}, valid_session
+	delete :destroy, {:id => Event.last.to_param}
       end
 
       it "assigns a newly created event as @event" do
-        post :create, {:event => valid_attributes}, valid_session
+        post :create, {:event => valid_attributes}
         assigns(:event).should be_a(Event)
         assigns(:event).should be_persisted
 	#cleanup google calendar
-	delete :destroy, {:id => Event.last.to_param}, valid_session
+	delete :destroy, {:id => Event.last.to_param}
       end
 
       it "redirects to the created event" do
-        post :create, {:event => valid_attributes}, valid_session
+        post :create, {:event => valid_attributes}
         response.should redirect_to(Event.last)
 	#cleanup google calendar
-	delete :destroy, {:id => Event.last.to_param}, valid_session
+	delete :destroy, {:id => Event.last.to_param}
       end
     end
 
@@ -75,14 +75,14 @@ describe EventsController do
       it "assigns a newly created but unsaved event as @event" do
         # Trigger the behavior that occurs when invalid params are submitted
         Event.any_instance.stub(:save).and_return(false)
-        post :create, {:event => {}}, valid_session
+        post :create, {:event => {}}
         assigns(:event).should be_a_new(Event)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         Event.any_instance.stub(:save).and_return(false)
-        post :create, {:event => {}}, valid_session
+        post :create, {:event => {}}
         response.should render_template("new")
       end
     end
@@ -97,33 +97,33 @@ describe EventsController do
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
         Event.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => event.to_param, :event => {'these' => 'params'}}, valid_session
+        put :update, {:id => event.to_param, :event => {'these' => 'params'}}
       end
 
       it "assigns the requested event as @event" do
-	post :create, {:event => valid_attributes}, valid_session
+	post :create, {:event => valid_attributes}
         event = Event.last
-        put :update, {:id => event.to_param, :event => valid_attributes}, valid_session
+        put :update, {:id => event.to_param, :event => valid_attributes}
         assigns(:event).should eq(event)
 	#cleanup google calendar
-	delete :destroy, {:id => event.to_param}, valid_session
+	delete :destroy, {:id => event.to_param}
       end
 
       it "redirects to the event" do
-        post :create, {:event => valid_attributes}, valid_session
+        post :create, {:event => valid_attributes}
         event = Event.last
-        put :update, {:id => event.to_param, :event => valid_attributes}, valid_session
+        put :update, {:id => event.to_param, :event => valid_attributes}
         response.should redirect_to(event)
 	#cleanup google calendar
-	delete :destroy, {:id => event.to_param}, valid_session
+	delete :destroy, {:id => event.to_param}
       end
 
       it "it creates a new gcal event since event didn't exist" do
 	event = Event.create! valid_attributes
-        put :update, {:id => event.to_param, :event => valid_attributes}, valid_session
+        put :update, {:id => event.to_param, :event => valid_attributes}
         response.should redirect_to(Event.last)
 	#cleanup google calendar
-	delete :destroy, {:id => event.to_param}, valid_session
+	delete :destroy, {:id => event.to_param}
       end
     end
 
@@ -132,7 +132,7 @@ describe EventsController do
         event = Event.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Event.any_instance.stub(:save).and_return(false)
-        put :update, {:id => event.to_param, :event => {}}, valid_session
+        put :update, {:id => event.to_param, :event => {}}
         assigns(:event).should eq(event)
       end
 
@@ -140,7 +140,7 @@ describe EventsController do
         event = Event.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Event.any_instance.stub(:save).and_return(false)
-        put :update, {:id => event.to_param, :event => {}}, valid_session
+        put :update, {:id => event.to_param, :event => {}}
         response.should render_template("edit")
       end
     end
@@ -148,24 +148,24 @@ describe EventsController do
 
   describe "DELETE destroy" do
     it "destroys the requested event" do
-      post :create, {:event => valid_attributes}, valid_session
+      post :create, {:event => valid_attributes}
       event = Event.last
       expect {
-        delete :destroy, {:id => event.to_param}, valid_session
+        delete :destroy, {:id => event.to_param}
       }.to change(Event, :count).by(-1)
     end
 
     it "redirects to the events list" do
-      post :create, {:event => valid_attributes}, valid_session
+      post :create, {:event => valid_attributes}
       event = Event.last
-      delete :destroy, {:id => event.to_param}, valid_session
+      delete :destroy, {:id => event.to_param}
       response.should redirect_to(events_url)
     end
 
     it "fails to destroy the event since it doesn't exist" do
       begin
       	event = Event.create! valid_attributes
-      	delete :destroy, {:id => event.to_param}, valid_session
+      	delete :destroy, {:id => event.to_param}
       	response.should raise_error
       rescue; end
     end

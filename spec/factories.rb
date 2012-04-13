96630dc7ca9b5e66a6b6e4d64d9df3fe842dc90a
David Mayer <djmayer101@gmail.com>
2012-04-12 20:45:42 -0500

    fixed rspec tests and added factorygirl for duties

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,19 @@ FactoryGirl.define do
    last_name   "Dabolina"
    admin    true
   end
+
+  factory :user_with_duty, :parent => :user do
+    after_create do |u|
+	FactoryGirl.create(:duty, :user => u)
+    end
+  end
+
+  factory :duty, class: Duty do 
+   title	"my Duty"
+   description	"clean"
+   deadline	Date.today
+   user_id	0 #user_id_parm
+   penalty	"pay $5"
+   confirmation	false
+  end
 end

8f184760c6a3f819847545a89aecbe3dcd5c1b27
Brennan Payne <brennan@brennan-Studio-1555.(none)>
2012-05-03 17:06:47 -0500

    Made position attributes accessible so we can actually create positions

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,5 +1,6 @@
 class Position < ActiveRecord::Base
   #has_many  :users, :through => :position_assignments
+  attr_accessible :title, :description, :addEvents, :addDuties, :addItems, :user_ids
   has_and_belongs_to_many :users
   validates :title, :presence => true
 end

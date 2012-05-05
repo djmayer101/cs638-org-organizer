7fc9615a8f0fdf098729d49db401ca9d9ca3f340
David Mayer <djmayer101@gmail.com>
2012-05-05 00:23:02 -0500

    added date validation to duties as well

--- app/models/duty.rb
+++ app/models/duty.rb
@@ -3,5 +3,8 @@ class Duty < ActiveRecord::Base
   belongs_to :assigned_user, :class_name => "User", :foreign_key => 'assigner_id' # user who created the duty
   has_and_belongs_to_many :users
   validates :title, :penalty, :presence=> true;
+
+	validates :deadline,
+  	:date => { :after => DateTime.now - 1.day }
   attr_accessible :title, :description, :deadline, :penalty, :confirmation, :user_ids, :assigned_user_id
 end
